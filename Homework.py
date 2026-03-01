56c406550152c39e58a09c0b75940cf8c402ae97
Maxim Dobinda <maxim.benid@gmail.com>
2026-03-01 14:35:54 +0200

    do homework

---
+++ Homework.py
@@ -0,0 +1,34 @@
+# a = 3
+# b = 9
+# print("Numerele de la a la b: ", end = '')
+# for num in range(a, b + 1):
+#     print(num, end = ' ')
+
+# a = int(input("Introduceti numarul 'a': "))
+# b = int(input("Introduceti numarul 'b': "))
+#
+# if a > b:
+#     for num in range(a, b - 1, -1):
+#         print(num, end = ' ')
+# elif a < b:
+#     for num in range(a, b + 1):
+#         print(num, end = ' ')
+
+
+# a = int(input('Enter first number: '))
+# b = int(input('Enter second number: '))
+# suma = 0
+# for i in range(a, b+1):
+#     suma += i
+# print(suma)
+
+# import random
+# secret = random.randrange(1,10)
+# guess = None
+# while guess != secret:
+#     guess = int(input("Ghiceste numarul: "))
+# print('Corect!')
+
+
+
+
