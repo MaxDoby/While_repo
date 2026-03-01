5f621aac7b67b427f724dc443adb9ff5b44bd95a
Maxim Dobinda <maxim.benid@gmail.com>
2026-03-01 12:22:17 +0200

    add datetime.py

--- While_1.py
+++ While_1.py
@@ -1,3 +1,20 @@
-parola = ''
-while parola != 'admin':
-    parola = input('Introdu parola: ')
+# parola = ''
+# while parola != 'admin':
+#     parola = input('Introdu parola: ')
+#
+#
+# #Bucla infinita
+# x = 0
+# while x <5:
+#     print(x)
+
+#bucla cu schimbarea ocnditiei
+# x = 0
+# while x < 5:
+#     print(x)
+#     #x += 1
+#     x = x + 1
+#
+# number = input('Introduceti un numar de la 1 - 10: ')
+# while number > '10' and number != '0':
+#     number = input('Introduceti un numar de la 1 - 10: ')
---
+++ datetime_2.py
@@ -0,0 +1,26 @@
+# from datetime import datetime
+# print(datetime.now())
+
+# from datetime import datetime
+# now = datetime.now()
+# current_year  = now.year
+# current_month = now.month
+# current_day   = now.day
+#
+# print(current_year)
+# print(current_month)
+# print(current_day)
+
+
+# from datetime import datetime
+# new = datetime.now()
+#
+# print('%s-%s-%s' % (new.year, new.month, new.day))
+
+from datetime import datetime
+now = datetime.now()
+
+print('%s:%s:%s' % (now.hour, now.minute, now.second))
+print(now.hour)
+print(now.minute)
+print(now.second)
